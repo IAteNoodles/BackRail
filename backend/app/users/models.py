bf40f145cf63535459cc0d5f00d644fd76413aea
IAteNoodles <90202919+IAteNoodles@users.noreply.github.com>
2026-02-23 19:04:05 +0530

    Email and Phone no is not all you need!!!

--- backend/app/users/models.py
+++ backend/app/users/models.py
@@ -4,19 +4,18 @@ from django.contrib.auth.models import BaseUserManager, AbstractUser
 # Create your models here.
 
 class UserManager(BaseUserManager):
-    def create_user(self, HRMS_ID, email, phone_no=None, password = None, **extra_fields):
+    def create_user(self, HRMS_ID, email=None, phone_no=None, password = None, **extra_fields):
         if not HRMS_ID:
             raise ValueError("Users must have an HRMS ID")
-        if not email:
-            raise ValueError("Users must have an email address")
-        email = self.normalize_email(email)
+        if email:
+            email = self.normalize_email(email)
         
         user = self.model(HRMS_ID=HRMS_ID, email=email, phone_number=phone_no, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, HRMS_ID, email, password, phone_no=None, **extra_fields):
+    def create_superuser(self, HRMS_ID, email=None, password=None, phone_no=None, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
         return self.create_user(HRMS_ID, email, phone_no, password, **extra_fields)
@@ -24,8 +23,8 @@ class UserManager(BaseUserManager):
 class User(AbstractUser):
     username = None
     HRMS_ID = models.CharField(max_length=255, unique=True)
-    email = models.EmailField(unique=True)
-    phone_number = models.CharField(max_length=10, blank=True, null=True, unique=True)
+    email = models.EmailField(unique=True, blank=True, null=True, default=None)
+    phone_number = models.CharField(max_length=10, blank=True, null=True, unique=True, default=None)
     USERNAME_FIELD = 'HRMS_ID'
     REQUIRED_FIELDS = ['email']
     objects = UserManager()
--- backend/app/users/serialzers.py
+++ backend/app/users/serialzers.py
@@ -4,6 +4,7 @@ from .models import User
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
     phone_no = serializers.CharField(source='phone_number', required=False)
+    email = serializers.EmailField(required=False)
 
     class Meta:
         model = User
@@ -12,8 +13,8 @@ class RegisterSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         user = User.objects.create_user(
             HRMS_ID = validated_data['HRMS_ID'],
-            email = validated_data['email'],
+            email = validated_data.get('email', None),
             password = validated_data['password'],
-            phone_no = validated_data.get('phone_number', '')
+            phone_no = validated_data.get('phone_number', None)
         )
         return user
